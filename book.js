11b324bd71f285aacfbbc2246fd38895ff6ac40b
wjh <wjhjavahaha@163.com>
2025-09-28 16:31:09 +0800

    Add package.json and book.js for GitBook deployment

---
+++ book.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    "-lunr",
+    "-search",
+    "search-pro",
+    "code",
+    "theme-lou",
+    "expandable-chapters",
+    "back-to-top-button"
+  ],
+  pluginsConfig: {
+    // "search-pro": {
+    //   "path": "search_plus_index.json"
+    // },
+    // "code": {
+    //   "theme": "github"
+    // }
+  }
+};
